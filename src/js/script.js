8a4d3e2eb4328b8ce897baca8b18f6c6fea85d9b
Maria <mashok.mo@gmail.com>
2020-12-14 21:05:01 +0300

    Add slider

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+$('.carousel__inner').slick({
+    adaptiveHeight: true,
+    speed: 700,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.svg"></button>'
+  });
