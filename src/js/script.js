bf4032606c88298929e6a58d8a1f2a5a5e819993
Maria <mashok.mo@gmail.com>
2020-12-15 00:24:41 +0300

    Add tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,28 @@
-$('.carousel__inner').slick({
-    adaptiveHeight: true,
-    speed: 700,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.svg"></button>'
-  });
+
+$(document).ready(function(){  
+	$('.carousel__inner').slick({
+			adaptiveHeight: true,
+			speed: 700,
+			prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.svg"></button>',
+			nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.svg"></button>'
+	});
+
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+	
+	function toggleSlide(item) {
+		$(item).each(function(i) {
+				$(this).on('click', function(e) {
+						e.preventDefault();
+						$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+						$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+				})
+		});
+	};
+
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+});
